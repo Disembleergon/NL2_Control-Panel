d663ef439beb40602323f80260b32fb38a9c4a1f
Disembleergon <tom.pekrul1@gmail.com>
2021-10-19 10:32:19 +0200

    finished menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,10 +2,18 @@ import React from "react";
 import "../styles/Menu.css";
 
 function Menu() {
+  let onBtnClick = () => {
+    let IP_adress = document.querySelector(".menuInput").innerHTML;
+    sessionStorage.setItem("nl2-control-panel_entered-ip", IP_adress);
+    window.open("/panel", "_self");
+  };
+
   return (
     <div className="menu">
       <input type="text" placeholder="IP-adress" className="menuInput"></input>
-      <button className="menuButton">CONNECT</button>
+      <button className="menuButton" onClick={onBtnClick}>
+        CONNECT
+      </button>
     </div>
   );
 }
